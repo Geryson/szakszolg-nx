c2e518e110da45b1468a497441b636c9d836745b
Dombi Tibor <dombidav@gmail.com>
2022-01-18 17:58:03 +0100

    Update run-scripts

--- libs/core/src/lib/core.module.ts
+++ libs/core/src/lib/core.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { configuration } from './config/configuration'
 import { validationSchema } from './config/validation'
+import { GraphQLModule } from '@nestjs/graphql'
+import { CoreResolver } from './core.resolver'
 
 @Module({
     imports: [
@@ -10,9 +12,13 @@ import { validationSchema } from './config/validation'
             load: [configuration],
             validationSchema,
         }),
+        GraphQLModule.forRoot({
+            autoSchemaFile: true,
+            playground: true,
+        }),
     ],
     controllers: [],
-    providers: [],
+    providers: [CoreResolver],
     exports: [],
 })
 export class CoreModule {}
