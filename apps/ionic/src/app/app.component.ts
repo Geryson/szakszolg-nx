f731f1f7b8e0d12147cd9b2a98113449f02cc561
Dombi Tibor <dombidav@gmail.com>
2022-02-02 12:50:07 +0100

    Installed PrimeNG

--- apps/ionic/src/app/app.component.ts
+++ apps/ionic/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { PrimeNGConfig } from 'primeng/api'
 
 @Component({
-  selector: 'nx12-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'nx12-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+    constructor(private primengConfig: PrimeNGConfig) {}
+
+    ngOnInit() {
+        this.primengConfig.ripple = true
+    }
 }
