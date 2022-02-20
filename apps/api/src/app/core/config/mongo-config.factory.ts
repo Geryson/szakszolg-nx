611c6f1183ce70826cfb373381d489609bdf6ebc
Molnár Dávid <davemill96@gmail.com>
2022-02-20 18:01:55 +0100

    Allow Mongoose to use usernam/password auth

--- apps/api/src/app/core/config/mongo-config.factory.ts
+++ apps/api/src/app/core/config/mongo-config.factory.ts
@@ -5,16 +5,28 @@ export const mongooseConfig = {
     imports: [ConfigModule],
     providers: [ConfigService],
     mongooseConfigFactory: async (configService: ConfigService): Promise<MongooseModuleOptions | any> => {
-        const uri = `mongodb+srv://${configService.get<string>('DATABASE_USER')}@${configService.get<string>(
-            'DATABASE_HOST',
-        )}/${configService.get<string>(
-            'DATABASE_NAME',
-        )}?authMechanism=MONGODB-X509&ssl=true&retryWrites=true&w=majority`
         const cert = configService.get<string>('DATABASE_CERT')
-        return {
-            uri,
-            sslKey: cert,
-            sslCert: cert,
+        let uri: string
+        if (cert) {
+            uri = `mongodb+srv://${configService.get<string>('DATABASE_USER')}@${configService.get<string>(
+                'DATABASE_HOST',
+            )}/${configService.get<string>(
+                'DATABASE_NAME',
+            )}?authMechanism=MONGODB-X509&ssl=true&retryWrites=true&w=majority`
+            return {
+                uri,
+                sslKey: cert,
+                sslCert: cert,
+            }
+        } else {
+            uri = `mongodb+srv://${configService.get<string>('DATABASE_USER')}:${configService.get<string>('DATABASE_PASSWORD')}@${configService.get<string>(
+                'DATABASE_HOST',
+            )}/${configService.get<string>(
+                'DATABASE_NAME',
+            )}?retryWrites=true&w=majority`
+            return {
+                uri
+            }
         }
     },
 }
