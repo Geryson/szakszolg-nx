abbe061a95341310c4c15e75067d6b44a9bc23a0
Dombi Tibor <dombidav@gmail.com>
2022-02-28 21:35:17 +0100

    Implemented Browse on manage-puzzles page

--- apps/ionic/src/app/admin/manage-puzzles/manage-puzzles.module.ts
+++ apps/ionic/src/app/admin/manage-puzzles/manage-puzzles.module.ts
@@ -1,20 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular'
 
-import { ManagePuzzlesPageRoutingModule } from './manage-puzzles-routing.module';
+import { ManagePuzzlesPageRoutingModule } from './manage-puzzles-routing.module'
 
-import { ManagePuzzlesPage } from './manage-puzzles.page';
+import { ManagePuzzlesPage } from './manage-puzzles.page'
+import { NxSharedModule } from '../../../shared/nx-shared.module'
+import { SharedModule } from 'primeng/api'
+import { DataViewModule } from 'primeng/dataview'
+import { DropdownModule } from 'primeng/dropdown'
+import { InputTextModule } from 'primeng/inputtext'
+import { ButtonModule } from 'primeng/button'
+import { TranslateModule } from '@ngx-translate/core'
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ManagePuzzlesPageRoutingModule
-  ],
-  declarations: [ManagePuzzlesPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ManagePuzzlesPageRoutingModule,
+        NxSharedModule,
+        SharedModule,
+        DataViewModule,
+        DropdownModule,
+        InputTextModule,
+        ButtonModule,
+        TranslateModule,
+    ],
+    declarations: [ManagePuzzlesPage],
 })
 export class ManagePuzzlesPageModule {}
--- apps/ionic/src/app/admin/manage-puzzles/manage-puzzles.page.ts
+++ apps/ionic/src/app/admin/manage-puzzles/manage-puzzles.page.ts
@@ -1,15 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { IFile } from '@szakszolg-nx/api-interfaces'
+import { environment } from '../../../environments/environment'
+import { first } from 'rxjs'
+import { ConfirmationService, SelectItem } from 'primeng/api'
+import { TranslatePipe } from '@ngx-translate/core'
+import { NG_ICON } from '../../../shared/utils/prime-icons.class'
 
 @Component({
-  selector: 'nx12-manage-puzzles',
-  templateUrl: './manage-puzzles.page.html',
-  styleUrls: ['./manage-puzzles.page.scss'],
+    selector: 'nx12-manage-puzzles',
+    templateUrl: './manage-puzzles.page.html',
+    styleUrls: ['./manage-puzzles.page.scss'],
 })
 export class ManagePuzzlesPage implements OnInit {
+    NG_ICON = NG_ICON
 
-  constructor() { }
+    files: IFile[] = []
+    sortOptions: SelectItem[] = []
+    sortOrder = 0
+    sortField = 'path'
 
-  ngOnInit() {
-  }
+    constructor(
+        private readonly http: HttpClient,
+        private readonly translate: TranslatePipe,
+        private readonly confirm: ConfirmationService,
+    ) {
+        setTimeout(() => {
+            ;[
+                { label: this.translate.transform('PUZZLE_MANAGEMENT.CREATED_AT'), value: '!createdAt' },
+                { label: this.translate.transform('PUZZLE_MANAGEMENT.CREATED_AT_DESC'), value: 'createdAt' },
+                { label: this.translate.transform('PUZZLE_MANAGEMENT.NAME'), value: '!path' },
+                { label: this.translate.transform('PUZZLE_MANAGEMENT.NAME_DESC'), value: 'path' },
+                { label: this.translate.transform('PUZZLE_MANAGEMENT.SIZE'), value: '!size' },
+                { label: this.translate.transform('PUZZLE_MANAGEMENT.SIZE_DESC'), value: 'size' },
+            ].forEach((o) => this.sortOptions.push(o))
+        }, 1000)
+    }
 
+    private static get api() {
+        return `http${environment.API_SSL ? 's' : ''}://${environment.API_HOST}:${environment.API_PORT}/api/file`
+    }
+
+    private static getImageID(file: IFile) {
+        return file.path.replace(/uploads/g, '').replace(/\/\//g, '/')
+    }
+
+    getImageUrl(file: IFile) {
+        return `${ManagePuzzlesPage.api}/${ManagePuzzlesPage.getImageID(file)}?size=thumbnail`
+    }
+
+    getImageName(file: IFile) {
+        return file.path
+            .replace(/uploads/g, '')
+            .replace(/\/\//g, '/')
+            .replace(/--.+\./g, '.')
+            .substring(2)
+    }
+
+    ngOnInit() {
+        this.init()
+    }
+
+    onSortChange(event: any) {
+        let value = event.value
+
+        if (value.indexOf('!') === 0) {
+            this.sortOrder = -1
+            this.sortField = value.substring(1, value.length)
+        } else {
+            this.sortOrder = 1
+            this.sortField = value
+        }
+    }
+
+    destroy(item: IFile) {
+        this.http
+            .delete(this.getImageUrl(item))
+            .pipe(first())
+            .subscribe(() => this.init())
+    }
+
+    private init() {
+        this.http
+            .get<IFile[]>(ManagePuzzlesPage.api)
+            .pipe(first())
+            .subscribe((files) => (this.files = files))
+    }
 }
