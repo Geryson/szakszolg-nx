5d9fb5148ed60653db987b319cc911ad1edca22b
mmark99 <m.mark09@hotmail.com>
2022-03-02 15:34:53 +0100

    WIP

--- apps/ionic/src/app/guest/play-groups/play-groups.module.ts
+++ apps/ionic/src/app/guest/play-groups/play-groups.module.ts
@@ -7,14 +7,22 @@ import { IonicModule } from '@ionic/angular';
 import { PlayGroupsPageRoutingModule } from './play-groups-routing.module';
 
 import { PlayGroupsPage } from './play-groups.page';
+import {NxSharedModule} from "../../../shared/nx-shared.module";
+import {DragDropModule} from "primeng/dragdrop";
+import {PanelModule} from "primeng/panel";
+import {TableModule} from "primeng/table";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    PlayGroupsPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        PlayGroupsPageRoutingModule,
+        NxSharedModule,
+        DragDropModule,
+        PanelModule,
+        TableModule
+    ],
   declarations: [PlayGroupsPage]
 })
 export class PlayGroupsPageModule {}
--- apps/ionic/src/app/guest/play-groups/play-groups.page.ts
+++ apps/ionic/src/app/guest/play-groups/play-groups.page.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertService} from "../../../shared/services/alert.service";
+import {QueryRef} from "apollo-angular";
+import {IGroupingItem, IMirrorWord} from "@szakszolg-nx/api-interfaces";
+import {EmptyObject} from "apollo-angular/build/types";
+import {Subscription} from "rxjs";
+import {GroupingItemService} from "../../../shared/services/grouping-item.service";
 
 @Component({
   selector: 'nx12-play-groups',
   templateUrl: './play-groups.page.html',
   styleUrls: ['./play-groups.page.scss'],
 })
-export class PlayGroupsPage implements OnInit {
+export class PlayGroupsPage {
 
-  constructor() { }
+    correct?: string
+    groups?: string[]
+    word?: string
+    private queryRef?: QueryRef<{ groupingItem: Partial<IGroupingItem> }, EmptyObject>
+    private sub?: Subscription
+    selectedGroup?: string
+    private draggedWord?: string | null;
+    guessedAnswer?: boolean;
 
-  ngOnInit() {
-  }
+    constructor(private readonly service: GroupingItemService,private readonly alert: AlertService) { }
+
+    async init() {
+        const loading = await this.alert.loading('MESSAGE.LOADING')
+        this.queryRef = this.service.random()
+        this.sub = this.queryRef.valueChanges
+            .subscribe(
+                (res) =>
+                    (this.word = res.data.groupingItem.item ?? '!',
+                    this.correct = res.data.groupingItem.correct ?? '!',
+                    this.groups = res.data.groupingItem.groups ?? [])
+            )
+        loading.dismiss().then()
+    }
+
+    ionViewDidEnter(): void {
+        this.init().then()
+    }
+
+    dragStart(word: string | undefined) {
+        this.draggedWord = word
+    }
+
+    drop(group: string) {
+        if(this.draggedWord){
+            this.selectedGroup = group
+            this.guessedAnswer = group === this.correct;
+            this.draggedWord = null
+        }
+    }
+
+    dragEnd() {
+        this.draggedWord = null
+    }
 
 }
