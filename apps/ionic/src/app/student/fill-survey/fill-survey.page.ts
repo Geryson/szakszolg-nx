e52bebe4ac8f67373ec4c8657532e6dfd6548ab4
mmark99 <m.mark09@hotmail.com>
2022-03-07 17:28:21 +0100

    Added redirect if token is empty

--- apps/ionic/src/app/student/fill-survey/fill-survey.page.ts
+++ apps/ionic/src/app/student/fill-survey/fill-survey.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {SurveyService} from "../../../shared/services/survey.service";
 import {TokenService} from "../../../shared/services/token.service";
 import {IQuizQuestion} from "@szakszolg-nx/api-interfaces";
+import {RedirectService} from "../../../shared/services/redirect.service";
+import {pages} from "../../../shared/utils/pages.const";
 
 @Component({
   selector: 'nx12-fill-survey',
@@ -12,9 +14,11 @@ export class FillSurveyPage{
 
     public index = [1,2,3,4,5]
     public questions?: IQuizQuestion[] | undefined
-  constructor(protected readonly service: TokenService) { }
+  constructor(protected readonly service: TokenService, private readonly redirect: RedirectService) { }
 
     ionViewDidEnter(){
+        if(!this.questions)
+            this.redirect.to(pages.student.enterToken)
         this.questions = this.service.activeQuiz?.questions
     }
 }
