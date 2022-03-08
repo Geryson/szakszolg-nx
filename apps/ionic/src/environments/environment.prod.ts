c8a7d9220dd2ddeb149b77098210a26648ca9739
Dombi Tibor <dombidav@gmail.com>
2022-03-08 09:47:42 +0100

    Added certified backend route

--- apps/ionic/src/environments/environment.prod.ts
+++ apps/ionic/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_HOST: '165.232.86.205',
-    API_PORT: '3000',
-    API_SSL: false,
+    API_HOST: 'justnop.xyz',
+    API_PORT: '',
+    API_SSL: true,
 }
