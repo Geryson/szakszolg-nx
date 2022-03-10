8185132d204bd84e38698835243c723f7904ad46
bogosbalint <bogosbalint2001@gmail.com>
2022-03-10 11:10:16 +0100

    Added scss to rating

--- apps/ionic/src/shared/nx-shared.module.ts
+++ apps/ionic/src/shared/nx-shared.module.ts
@@ -19,12 +19,11 @@ import {AnswerChooseComponent} from "../app/student/fill-survey/answer-choose/an
 import {RadioButtonModule} from "primeng/radiobutton";
 import {FormsModule} from "@angular/forms";
 import {AnswerFreeAnswerComponent} from "../app/student/fill-survey/answer-free-answer/answer-free-answer.component";
-import {AnswerCustomComponent} from "../app/student/fill-survey/answer-custom/answer-custom.component";
 import {InputTextareaModule} from "primeng/inputtextarea";
 import {ButtonModule} from "primeng/button";
 
 @NgModule({
-    declarations: [HeaderComponent, JoinPipe, AnswerChooseComponent, AnswerFreeAnswerComponent, AnswerCustomComponent],
+    declarations: [HeaderComponent, JoinPipe, AnswerChooseComponent, AnswerFreeAnswerComponent],
     imports: [CommonModule, IonicModule, TranslateModule, RadioButtonModule, FormsModule, InputTextareaModule, ButtonModule],
     providers: [
         AlertService,
@@ -38,6 +37,6 @@ import {ButtonModule} from "primeng/button";
         SurveyService,
         UserService,
     ],
-    exports: [HeaderComponent, JoinPipe, AnswerChooseComponent, AnswerFreeAnswerComponent, AnswerCustomComponent],
+    exports: [HeaderComponent, JoinPipe, AnswerChooseComponent, AnswerFreeAnswerComponent],
 })
 export class NxSharedModule {}
