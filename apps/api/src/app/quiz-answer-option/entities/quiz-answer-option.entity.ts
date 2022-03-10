b74cc4d4ebd26a1f37bb0f81d289cfb609e17c91
Dombi Tibor <dombidav@gmail.com>
2022-03-10 10:04:30 +0100

    [FIX] Changed _id type to number in quiz-question and quiz-answer-option

--- apps/api/src/app/quiz-answer-option/entities/quiz-answer-option.entity.ts
+++ apps/api/src/app/quiz-answer-option/entities/quiz-answer-option.entity.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType } from '@nestjs/graphql'
+import { Field, Int, ObjectType } from '@nestjs/graphql'
 import { IQuizAnswerOption } from '@szakszolg-nx/api-interfaces'
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { Schema as MongooseSchema, Document } from 'mongoose'
@@ -8,8 +8,8 @@ export type QuizAnswerOptionDocument = IQuizAnswerOption & Document
 @ObjectType()
 @Schema()
 export class QuizAnswerOption implements IQuizAnswerOption {
-    @Field(() => String)
-    @Prop({ type: MongooseSchema.Types.ObjectId })
+    @Field(() => Int)
+    @Prop({ type: MongooseSchema.Types.Mixed })
     _id: any
 
     @Field()
--- apps/api/src/app/quiz-question/dto/inputs/update-quiz-question.input.ts
+++ apps/api/src/app/quiz-question/dto/inputs/update-quiz-question.input.ts
@@ -1,14 +1,12 @@
 import { IdInput } from '../../../../shared/gql-inputs/id.input'
 import { Field, InputType } from '@nestjs/graphql'
-import { IsNotEmpty } from 'class-validator'
 import { IQuizAnswerOption, IUpdatedAt } from '@szakszolg-nx/api-interfaces'
 import { UpdateQuizAnswerOptionInput } from '../../../quiz-answer-option/dto/inputs/update-quiz-answer-option.input'
 
 @InputType()
 export class UpdateQuizQuestionInput extends IdInput implements IUpdatedAt {
-    @Field()
-    @IsNotEmpty()
-    id: string
+    @Field({ nullable: true })
+    _id: number
 
     @Field({ nullable: true })
     question: string
