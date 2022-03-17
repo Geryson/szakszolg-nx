dc777677e2f6d45a53d46784ceed52ac0894fc5f
Nagy Ákos <nagyakos19971127@gmail.com>
2022-03-17 10:57:47 +0100

    Added CreateQuizAnswerInput interface

---
+++ libs/api-interfaces/src/lib/create-quiz-answer-input.interface.ts
@@ -0,0 +1,10 @@
+
+export interface ICreateQuizAnswerInput {
+    quizId: string
+    token: string | null
+    questionId: number
+    answer: string
+    om: string
+    answeredAt?: Date
+    isCorrect?: boolean | null
+}
