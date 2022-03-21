b9462a628bb4caa128ec1f93efcf7289a77e26c3
bogosbalint <bogosbalint2001@gmail.com>
2022-03-21 17:47:25 +0100

    Made a function for the true-false answers

--- apps/ionic/src/app/admin/manage-surveys/manage-single-survey/question-edit-form/question-edit-form.component.ts
+++ apps/ionic/src/app/admin/manage-surveys/manage-single-survey/question-edit-form/question-edit-form.component.ts
@@ -25,24 +25,7 @@ export class QuestionEditFormComponent {
             Log.debug('QuestionEditFormComponent::constructor()', 'template', this.template)
             switch (this.template) {
                 case 'true-false':
-                    if(this.question.answers!.length < 1){
-                        console.log('Bement')
-                        this.question.answers!.push({
-                            _id: this.question.answers!.length,
-                            text: 'Igaz',
-                            categoryIndex: 0,
-                            isCorrect: false,
-                            createdAt: new Date(),
-                        })
-
-                        this.question.answers!.push({
-                            _id: this.question.answers!.length,
-                            text: 'Hamis',
-                            categoryIndex: 0,
-                            isCorrect: false,
-                            createdAt: new Date(),
-                        })
-                    }
+                    this.FixForTrueFalse();
                     this.question.type = 'true-false'
                     break
                 case 'quiz':
@@ -50,6 +33,28 @@ export class QuestionEditFormComponent {
                     break
             }
         }, 100)
+    }
+
+    private FixForTrueFalse() {
+        if(!this.question.answers) return
+
+        if (this.question.answers.length < 1) {
+            this.question.answers.push({
+                _id: this.question.answers.length,
+                text: 'Igaz',
+                categoryIndex: 0,
+                isCorrect: false,
+                createdAt: new Date(),
+            })
+
+            this.question.answers.push({
+                _id: this.question.answers.length,
+                text: 'Hamis',
+                categoryIndex: 0,
+                isCorrect: false,
+                createdAt: new Date(),
+            })
+        }
     }
 
     get templateKey() {
