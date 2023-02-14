142701b94b39cf0412a4ef45aeff45cef7b29694
Csávás Gergely <csavas.g95@gmail.com>
2023-02-14 14:15:34 +0100

    - Iskolakeresőben lévő gombok nevének megváltoztatása, kapcsolódó elérési utak megadása - network_security_config beállítások elkészítése, kapcsolódó attribútum hozzáadva az AndroidManifest fájlban - Alkalmazás első éles azonosítója és neve beírva a config fájlokba - Backend régi elérési útjának korábbi definíciói törölve, éles szerver elérése beállítva - Play Store alkalmazás készítéséhez szükséges alapértelmezett ikon lecserélése az alkalmazás logójára (minden szükséges méretben és formátumban) - App indulásakor látható nagy kép (splash icon) eltüntetése

--- apps/ionic/src/environments/environment.prod.ts
+++ apps/ionic/src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 
 export const environment = {
-    production: true,
-    API_HOST: 'app.hmpedszakszolgalat.hu',
-    API_PORT: '3000',
-    API_SSL: false,
- }
- 
+   production: true,
+   API_HOST: 'app.hmpedszakszolgalat.hu',
+   API_PORT: '3000',
+   API_SSL: true,
+}
